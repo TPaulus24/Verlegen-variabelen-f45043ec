5ec401df843aff61f206562efa32bbcd6e1799ca
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2020-02-03 15:15:53 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function myFunctionBereken() {
-    let diameter = document.getElementById("diameter").value;
-    let perimeter = (Math.PI * diameter).toFixed(2);
-    let surface = (0.25 * Math.PI * Math.pow(diameter, 2)).toFixed(2);
-    document.getElementById("uitkomst").innerHTML = "De omtrek van de cirkel is " + perimeter + "<br>De oppervlakte van de cirkel is " + surface;
 }
